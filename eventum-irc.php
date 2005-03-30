09dce7d601057e4557ae92ceb3ad4203d171c975
Elan Ruusamäe <glen@pld-linux.org>
2005-03-30 09:04:00 +0000

    - $irc_password support
    Changed files:
        eventum-irc-config.patch -> 1.4
        eventum-irc.php -> 1.3

--- eventum-irc.php
+++ eventum-irc.php
@@ -6,6 +6,7 @@ $irc_port = 6667;
 $irc_nick = 'EventumBot';
 $irc_realname = 'EventumBot';
 $irc_username = 'EventumBot';
+$irc_password = '';
 
 $irc_default_channel = '#issues';
 $irc_default_project = 'Default Project';
