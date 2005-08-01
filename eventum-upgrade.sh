f44acf2a9ac1e4b758d70d3b2a76a57f66595def
Elan Ruusamäe <glen@pld-linux.org>
2005-08-01 12:42:40 +0000

    - remove sleep, we have now the needed privs granted by setup script
    Changed files:
        eventum-upgrade.sh -> 1.2

--- eventum-upgrade.sh
+++ eventum-upgrade.sh
@@ -6,14 +6,8 @@ if [ ! -d "$scriptdir" ]; then
 	exit 1
 fi
 
-cat >&2 <<-EOF
-Running eventum upgrade scripts to in $scriptdir
-These will fail if your eventum user doesn't have ALTER privilege to database.
+echo >&2 "Running eventum upgrade scripts to in $scriptdir"
 
-!!! Proceeding in 10 seconds !!!
-EOF
-
-sleep 10s
 if [ -x /usr/bin/php4 ]; then
 	php=/usr/bin/php4
 else
@@ -27,6 +21,7 @@ upgrade_script() {
 	if ! $php -q $scriptdir/$script; then
 		echo >&2 ""
 		echo >&2 "Please run manually: $php -q $scriptdir/$script"
+		echo >&2 "NOTE: You might need to add CREATE, DROP, ALTER and INDEX privileges to Eventum MySQL user for this to work".
 	fi
 	echo ""
 }
