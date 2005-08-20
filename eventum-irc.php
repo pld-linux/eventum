1105b2cd4a01311dccda43decb22d9f46dbcbc03
Elan Ruusamäe <glen@pld-linux.org>
2005-08-20 11:35:29 +0000

    - updated to 1.6.1
    Changed files:
        eventum-irc-config.patch -> 1.6
        eventum-irc.php -> 1.4

--- eventum-irc.php
+++ eventum-irc.php
@@ -1,14 +1,14 @@
 <?php
 
-$irc_host = 'localhost';
-$irc_port = 6667;
-
-$irc_nick = 'EventumBot';
-$irc_realname = 'EventumBot';
-$irc_username = 'EventumBot';
-$irc_password = '';
+$irc_server_hostname = 'localhost';
+$irc_server_port = 6667;
 
 $irc_default_channel = '#issues';
 $irc_default_project = 'Default Project';
+
+$nickname = 'EventumBOT';
+$realname = 'Eventum Issue Tracking System';
+$username = '';
+$password = '';
 
 ?>
