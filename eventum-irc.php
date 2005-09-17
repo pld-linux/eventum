1c43249f17dbd75a72b6b8636018ee5439d3da9e
Elan Ruusamäe <glen@pld-linux.org>
2005-09-17 14:31:21 +0000

    - channel setup is now a array
    Changed files:
        eventum-irc.php -> 1.5

--- eventum-irc.php
+++ eventum-irc.php
@@ -3,11 +3,18 @@
 $irc_server_hostname = 'localhost';
 $irc_server_port = 6667;
 
-$irc_default_channel = '#issues';
-$irc_default_project = 'Default Project';
+// the following is the list of IRC channels that the bot should connect to,
+// and the associated project name
+$irc_channels = array(
+	# Project Name -> IRC Channel
+	'Default Project' => '#issues',
+);
 
 $nickname = 'EventumBOT';
 $realname = 'Eventum Issue Tracking System';
+
+// do you need a username/password to connect to this server? if
+// so, fill in the next two variables
 $username = '';
 $password = '';
 
